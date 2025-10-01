7d0158b628cf49fb0b9f56f9f5393a4ee0fe244f
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-10-01 13:18:19 -0300

    Renomeada interface IconeHabilidade

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,6 +6,11 @@ interface ItemHabilidade {
   descricao?: string;
 }
 
+interface IconeHabilidade {
+  imagem: string;
+  titulo: string;
+}
+
 interface ItemPortfolio {
   titulo: string;
   descricao: string;
@@ -54,93 +59,93 @@ export class App {
     },
   ];
 
-  public iconesHabilidades: ItemHabilidade[] = [
+  public iconesHabilidades: IconeHabilidade[] = [
     {
-      icone: 'https://skillicons.dev/icons?i=html&theme=dark',
+      imagem: 'https://skillicons.dev/icons?i=html&theme=dark',
       titulo: 'HTML',
     },
     {
-      icone: 'https://skillicons.dev/icons?i=css&theme=dark',
+      imagem: 'https://skillicons.dev/icons?i=css&theme=dark',
       titulo: 'CSS',
     },
     {
-      icone: 'https://skillicons.dev/icons?i=scss&theme=dark',
+      imagem: 'https://skillicons.dev/icons?i=scss&theme=dark',
       titulo: 'SCSS',
     },
     {
-      icone: 'https://skillicons.dev/icons?i=js&theme=dark',
+      imagem: 'https://skillicons.dev/icons?i=js&theme=dark',
       titulo: 'JavaScript',
     },
     {
-      icone: 'https://skillicons.dev/icons?i=ts&theme=dark',
+      imagem: 'https://skillicons.dev/icons?i=ts&theme=dark',
       titulo: 'TypeScript',
     },
     {
-      icone: 'https://skillicons.dev/icons?i=bootstrap&theme=dark',
+      imagem: 'https://skillicons.dev/icons?i=bootstrap&theme=dark',
       titulo: 'Bootstrap',
     },
     {
-      icone: 'https://skillicons.dev/icons?i=angular&theme=dark',
+      imagem: 'https://skillicons.dev/icons?i=angular&theme=dark',
       titulo: 'Angular',
     },
     {
-      icone: 'https://skillicons.dev/icons?i=rxjs&theme=dark',
+      imagem: 'https://skillicons.dev/icons?i=rxjs&theme=dark',
       titulo: 'RxJS',
     },
     {
-      icone: 'https://skillicons.dev/icons?i=net&theme=dark',
+      imagem: 'https://skillicons.dev/icons?i=net&theme=dark',
       titulo: '.NET',
     },
     {
-      icone: 'https://skillicons.dev/icons?i=cs&theme=dark',
+      imagem: 'https://skillicons.dev/icons?i=cs&theme=dark',
       titulo: 'C#',
     },
     {
-      icone: 'https://skillicons.dev/icons?i=selenium&theme=dark',
+      imagem: 'https://skillicons.dev/icons?i=selenium&theme=dark',
       titulo: 'Selenium',
     },
     {
-      icone: 'https://skillicons.dev/icons?i=cypress&theme=dark',
+      imagem: 'https://skillicons.dev/icons?i=cypress&theme=dark',
       titulo: 'Cypress',
     },
     {
-      icone: 'https://skillicons.dev/icons?i=sqlite&theme=dark',
+      imagem: 'https://skillicons.dev/icons?i=sqlite&theme=dark',
       titulo: 'SQLite',
     },
     {
-      icone: 'https://skillicons.dev/icons?i=postgres&theme=dark',
+      imagem: 'https://skillicons.dev/icons?i=postgres&theme=dark',
       titulo: 'Postgres',
     },
     {
-      icone: 'https://skillicons.dev/icons?i=redis&theme=dark',
+      imagem: 'https://skillicons.dev/icons?i=redis&theme=dark',
       titulo: 'Redis',
     },
     {
-      icone: 'https://skillicons.dev/icons?i=docker&theme=dark',
+      imagem: 'https://skillicons.dev/icons?i=docker&theme=dark',
       titulo: 'Docker',
     },
     {
-      icone: 'https://skillicons.dev/icons?i=powershell&theme=dark',
+      imagem: 'https://skillicons.dev/icons?i=powershell&theme=dark',
       titulo: 'PowerShell',
     },
     {
-      icone: 'https://skillicons.dev/icons?i=git&theme=dark',
+      imagem: 'https://skillicons.dev/icons?i=git&theme=dark',
       titulo: 'Git',
     },
     {
-      icone: 'https://skillicons.dev/icons?i=github&theme=dark',
+      imagem: 'https://skillicons.dev/icons?i=github&theme=dark',
       titulo: 'GitHub',
     },
     {
-      icone: 'https://skillicons.dev/icons?i=azure&theme=dark',
+      imagem: 'https://skillicons.dev/icons?i=azure&theme=dark',
       titulo: 'Azure',
     },
     {
-      icone: 'https://skillicons.dev/icons?i=visualstudio&theme=dark',
+      imagem: 'https://skillicons.dev/icons?i=visualstudio&theme=dark',
       titulo: 'Visual Studio',
     },
     {
-      icone: 'https://skillicons.dev/icons?i=vscode&theme=dark',
+      imagem: 'https://skillicons.dev/icons?i=vscode&theme=dark',
       titulo: 'VSCode',
     },
   ];
